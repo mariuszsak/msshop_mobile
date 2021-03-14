888a32f1966922def718f2fc6a765d1bc236612f
mariuszsak <mariuszsak@gmail.com>
2021-03-14 23:12:21 +0100

    Added some initial code

--- App.tsx
+++ App.tsx
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {ItemList} from "./src/components/ItemList";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+type MyState = {
+    isLoading: boolean
+    data: []
+}
+
+export default class App extends Component<{}, MyState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        this.fetchData().then(r => r);
+
+    }
+
+    fetchData = async () => {
+        const response = await fetch('http://192.168.0.2:3000/products')
+        const json = await response.json()
+        this.setState({
+            isLoading: false,
+            data: json
+        })
+    }
+
+    render() {
+        const {isLoading} = this.state;
+        if (!isLoading) {
+            return (
+                <View style={styles.container}>
+                    <ItemList data={this.state.data}/>
+                    <StatusBar style="auto" backgroundColor="#61dafb"/>
+                </View>
+            )
+        } else {
+            return (
+                <View style={styles.container}>
+                    <Text>Loading</Text>
+                    <StatusBar style="auto" backgroundColor="#61dafb"/>
+                </View>
+            )
+        }
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1
+    }
 });
---
+++ src/components/ItemList.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import {FlatList, StyleSheet, Text, View} from "react-native";
+
+
+export function ItemList(props: any) {
+
+    console.log(props.data);
+
+    return (
+        <View style={{flex: 1, width: '100%'}}>
+            <FlatList contentContainerStyle={styles.allItems}
+                      data={props.data}
+                      keyExtractor={(item, index) => index.toString()}
+                      renderItem={({item}) => (
+                          <>
+                              <Text style={styles.singleItem}>{item.id} </Text>
+                              <Text style={styles.singleItem}>{item.name}, {item.description}, {item.price}
+                              </Text>
+                          </>
+                      )}
+            />
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    allItems: {
+        backgroundColor: '#fff',
+        margin: 15
+    },
+    singleItem: {
+        marginTop: 50,
+        padding: 5,
+        backgroundColor: '#aabbee'
+    }
+});
