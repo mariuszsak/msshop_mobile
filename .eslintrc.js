bc1ff020f5b1dfd0f8ca1af60089da34c142d8ff
mariuszsak <mariuszsak@gmail.com>
2021-03-23 11:44:10 +0100

    Added eslint & prettier + config files

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    extends: [
+        '@react-native-community',
+        'airbnb-typescript',
+        'prettier'
+    ],
+    parserOptions: {
+        "project": ["tsconfig.json"]
+    }
+};
