64a5a558f163df5335fb237e54ffb2151f04ddd8
mariuszsak <mariuszsak@gmail.com>
2021-03-23 11:44:53 +0100

    File renamed

--- src/components/navigator/AppTabNavigator.tsx
+++ src/components/navigator/AppTabNavigator.tsx
@@ -1,23 +1,25 @@
-import React from "react";
-import {NavigationContainer} from '@react-navigation/native';
-import Home from "../../screens/home";
-import About from "../../screens/about";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../../screens/home';
+import About from '../../screens/about';
 
 const Tab = createBottomTabNavigator();
 
-export const AppNavigator = () => (
+export const AppTabNavigator = () => (
     <NavigationContainer>
-        <Tab.Navigator tabBarOptions={{
-            activeTintColor: 'orange',
-            inactiveTintColor: 'blue',
-            activeBackgroundColor: 'red',
-            labelStyle: {
-                fontSize: 16
-            }
-        }}>
-            <Tab.Screen name='Home' component={Home} />
-            <Tab.Screen name='About' component={About} />
+        <Tab.Navigator
+            tabBarOptions={{
+                activeTintColor: 'orange',
+                inactiveTintColor: 'blue',
+                activeBackgroundColor: 'red',
+                labelStyle: {
+                    fontSize: 16
+                }
+            }}
+        >
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="About" component={About} />
         </Tab.Navigator>
     </NavigationContainer>
 );
