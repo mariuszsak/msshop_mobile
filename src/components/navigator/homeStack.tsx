12b1d38fe132de1e96ab9b09c603069d881d79aa
mariuszsak <mariuszsak@gmail.com>
2021-03-30 17:17:48 +0200

    Navigator added

---
+++ src/components/navigator/homeStack.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import ProductDetail from "../../screens/productDetail";
+import Home from "../../screens/home";
+
+const HomeStack = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={Home} options={{ headerShown: false }}/>
+      <Stack.Screen name='ProductDetail' component={ProductDetail}/>
+    </Stack.Navigator>
+  );
+};
+
+export default HomeStack;
