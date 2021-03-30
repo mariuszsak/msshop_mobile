0e8a6d152bb39e8faa72c69cb5638cec4a72df5f
mariuszsak <mariuszsak@gmail.com>
2021-03-30 17:16:15 +0200

    Navigator added

--- src/components/navigator/AppTabNavigator.tsx
+++ src/components/navigator/AppTabNavigator.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from '../../screens/home';
-import About from '../../screens/about';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import About from "../../screens/about";
+import HomeStack from "./homeStack";
 
 const Tab = createBottomTabNavigator();
 
-export const AppTabNavigator = () => (
-    <NavigationContainer>
-        <Tab.Navigator
-            tabBarOptions={{
-                activeTintColor: 'orange',
-                inactiveTintColor: 'blue',
-                activeBackgroundColor: '#fff',
-                labelStyle: {
-                    fontSize: 16
-                },
-              tabStyle: {
-                  borderColor: 'navy',
-                borderTopWidth: 1
-              }
-            }}
-        >
-            <Tab.Screen name="Home" component={Home} options={{}}/>
-            <Tab.Screen name="About" component={About} />
-        </Tab.Navigator>
-    </NavigationContainer>
+const AppTabNavigator = () => (
+  <NavigationContainer>
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: "orange",
+        inactiveTintColor: "blue",
+        activeBackgroundColor: "#fff",
+        labelStyle: {
+          fontSize: 16
+        },
+        tabStyle: {
+          borderColor: "navy",
+          borderTopWidth: 1
+        }
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="About" component={About} />
+    </Tab.Navigator>
+  </NavigationContainer>
 );
+export default AppTabNavigator;
