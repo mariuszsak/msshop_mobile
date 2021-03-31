629a853047039d8dddbf74b674203bcb3e926e6f
mariuszsak <mariuszsak@gmail.com>
2021-03-31 23:17:04 +0200

    Context provider fixed

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import AppTabNavigator from "./src/components/navigator/AppTabNavigator";
-import { AppContext } from "./src/context";
+import { AppContext, ContextDefaultValue } from "./src/context";
 
 export default class App extends Component {
 
   render() {
     return (
-      <AppContext.Provider value={{ items: [] }}>
+      <AppContext.Provider value={ContextDefaultValue}>
         <AppTabNavigator />
       </AppContext.Provider>
     );
