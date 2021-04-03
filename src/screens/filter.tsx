34ff77f7c6142da2442b8df704a88a6cd3265742
mariuszsak <mariuszsak@gmail.com>
2021-04-03 19:07:46 +0200

    WIP

--- src/screens/filter.tsx
+++ src/screens/filter.tsx
@@ -4,30 +4,75 @@ import Logo from "../components/logo";
 import { CheckBox, Divider } from "react-native-elements";
 
 export default function Filter() {
+
+  const iconSize: number = 14;
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Logo />
       </View>
       <View style={styles.body}>
-        <Text>Filter</Text>
+        <Text>Show glasses by type:</Text>
         <View style={styles.checkboxes}>
-        <CheckBox
-          title='x'
-          checked={true}
-        />
-        <CheckBox
-          title='Click Here'
-          checked={true}
-        />
-        <CheckBox
-          title='Click Here'
-          checked={true}
-        />
+          <CheckBox
+            title="Show all"
+            size={iconSize}
+            checked={true}
+
+          />
+          <CheckBox
+            title="Eyeglasses"
+            size={iconSize}
+            checked={true}
+          />
+          <CheckBox
+            title="Sunglasses"
+            size={iconSize}
+            checked={true}
+          />
         </View>
-        <Divider style={{ backgroundColor: 'blue' }} />
+        <Text>Show glasses by gender:</Text>
+        <View style={styles.checkboxes}>
+          <CheckBox
+            title="Unisex"
+            size={iconSize}
+            checked={true}
+          />
+          <CheckBox
+            title="Male"
+            size={iconSize}
+            checked={true}
+          />
+          <CheckBox
+            title="Female"
+            size={iconSize}
+            checked={true}
+          />
+        </View>
+        <Text>Show glasses by brand:</Text>
         <CheckBox
-          title='Click Here'
+          title="Show all"
+          size={iconSize}
+          checked={true}
+        />
+        <CheckBox
+          title="Hugo Boss"
+          size={iconSize}
+          checked={true}
+        />
+        <CheckBox
+          title="Polaroid"
+          size={iconSize}
+          checked={true}
+        />
+        <CheckBox
+          title="Tom Ford"
+          size={iconSize}
+          checked={true}
+        />
+        <CheckBox
+          title="Ray-Ban"
+          size={iconSize}
           checked={true}
         />
       </View>
@@ -48,9 +93,11 @@ const styles = StyleSheet.create({
       flex: 9,
       backgroundColor: "#fff"
     },
-  checkboxes: {
-      flexDirection: "row"
-  }
+    checkboxes: {
+      flexDirection: "row",
+      paddingBottom:20,
+      justifyContent: "center"
+    }
   }
 );
 
