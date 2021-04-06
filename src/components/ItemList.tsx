6914f2008a04453dc3bcf90d85a88b76ee1e7d06
mariuszsak <mariuszsak@gmail.com>
2021-04-06 13:38:32 +0200

    Styles moved to global file

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import ProductDetail from "../screens/productDetail";
+import styles from "../styles/style";
 
 type Props = {
   onPress(): void
@@ -46,44 +47,3 @@ export function ItemList(props: any) {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  allItems: {
-    backgroundColor: "#fff",
-    margin: 5
-  },
-  singleItem: {
-    paddingTop: 5,
-    paddingBottom: 5,
-    marginTop: 5,
-    flexDirection: "row",
-    borderStyle: "solid",
-    borderWidth: 1,
-    borderColor: "red",
-    height: 200
-  },
-  img: {
-    resizeMode: "stretch",
-    width: "100%"
-  },
-  left: {
-    width: "50%",
-    borderWidth: 1
-  },
-  right: {
-    flexDirection: "column",
-    padding: 5
-  },
-  name: {
-    fontWeight: "bold",
-    fontSize: 20
-  },
-  price: {
-    fontWeight: "bold",
-    fontSize: 30
-  },
-  priceSmall: {
-    fontWeight: "bold",
-    fontSize: 20
-  }
-});
