24943a82a5e688469498df58a9af43abe6763e8a
mariuszsak <mariuszsak@gmail.com>
2021-04-06 13:39:46 +0200

    WIP

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,8 +1,19 @@
 import React, { createContext } from "react";
 
+type Brand = {
+  id: number,
+  name: string
+}
+
 type MyItem = {
+  id: number,
   name: string,
-  price: number
+  type: string,
+  gender: string,
+  description: string,
+  price: number,
+  brandId: number,
+  brand: Brand
 }
 
 export type ContextProps = {
@@ -12,32 +23,50 @@ export type ContextProps = {
 export const ContextDefaultValue: ContextProps = {
   items: [
     {
-      name: "test1",
-      price: 100
+      id: 1,
+      name: "Hugo Boss TEST 1",
+      type: "EYEGLASSES",
+      gender: "MALE",
+      description: "HB glasses model TEST1",
+      price: 800,
+      brandId: 2,
+      brand: {
+        id: 2,
+        name: "Hugo Boss"
+      }
     },
     {
-      name: "test2",
-      price: 100
-    }
+      id: 2,
+      name: "Hugo Boss TEST 2",
+      type: "EYEGLASSES",
+      gender: "UNISEX",
+      description: "HB glasses model TEST2",
+      price: 1000,
+      brandId: 2,
+      brand: {
+        id: 2,
+        name: "Hugo Boss"
+      }
+    },
   ]
 };
 
 export const AppContext = createContext<ContextProps>(ContextDefaultValue);
-
-export class ItemProvider extends React.Component {
-  updateItem = (newItem: any) => {
-    this.setState({ items: newItem });
-  };
-
-  state = {
-    items: []
-  };
-
-  render() {
-    return (
-      <AppContext.Provider value={this.state}>
-        {this.props.children}
-      </AppContext.Provider>
-    );
-  }
-}
+//
+// export class ItemProvider extends React.Component {
+//   updateItem = (newItem: any) => {
+//     this.setState({ items: newItem });
+//   };
+//
+//   state = {
+//     items: []
+//   };
+//
+//   render() {
+//     return (
+//       <AppContext.Provider value={this.state}>
+//         {this.props.children}
+//       </AppContext.Provider>
+//     );
+//   }
+// }
