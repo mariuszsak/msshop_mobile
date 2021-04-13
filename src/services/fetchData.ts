2a628503450bce1957108c8f19d219741812adc1
mariuszsak <mariuszsak@gmail.com>
2021-04-13 23:54:42 +0200

    Minor fixes

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,6 +1,4 @@
 export async function fetchData(endpoint: string): Promise<any> {
     const response = await fetch(`http://192.168.0.2:3000/${endpoint}`);
-    const json = await response.json();
-    return json;
+    return await response.json();
 }
-
