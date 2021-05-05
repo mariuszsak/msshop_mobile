22dabe8f02d0319536b0be903db47f2f8e849739
mariuszsak <mariuszsak@gmail.com>
2021-05-05 20:48:58 +0200

    Type added

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,4 +1,4 @@
 export async function fetchData(endpoint: string): Promise<any> {
-    const response = await fetch(`http://192.168.0.2:3000/${endpoint}`);
+    const response: Response = await fetch(`http://192.168.0.2:3000/${endpoint}`);
     return await response.json();
 }
