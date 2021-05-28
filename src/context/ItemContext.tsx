f869bd875dd7f7a7e84bf8ab015062fc0b3611e9
mariuszsak <mariuszsak@gmail.com>
2021-05-28 23:22:32 +0200

    WIP

--- src/context/ItemContext.tsx
+++ src/context/ItemContext.tsx
@@ -1,49 +1,74 @@
-import React, { createContext } from "react";
-import { MyItem } from "../interfaces/MyItem";
+import React, { createContext, Dispatch, SetStateAction, useContext, useState } from "react";
+import { GlassBrand, GlassGender, GlassItem, GlassSelections, GlassType, ProductType } from "../../types";
 
-export interface ContextProps {
-  items: MyItem[];
+type GlassContext = {
+  glassItems: GlassItem[];
+  setGlassItems: Dispatch<SetStateAction<GlassItem[]>>;
+  glassItemsByType: GlassType[];
+  setGlassItemsByType: Dispatch<SetStateAction<GlassType[]>>;
+  glassItemsByGender: GlassGender[];
+  setGlassItemsByGender: Dispatch<SetStateAction<GlassGender[]>>;
+  glassItemsByBrand: GlassBrand[];
+  setGlassItemsByBrand: Dispatch<SetStateAction<GlassBrand[]>>;
+  selectedGlasses: GlassSelections[];
+  setSelectedGlasses: Dispatch<SetStateAction<GlassSelections[]>>;
 }
 
-export const EmptyContextDefaultValue: ContextProps = {
-  items: []
+export const ContextDefaultValue: GlassContext = {
+  glassItems: [],
+  setGlassItems: () => {},
+  glassItemsByType: [],
+  setGlassItemsByType: () => {},
+  glassItemsByGender: [],
+  setGlassItemsByGender: () => {},
+  glassItemsByBrand: [],
+  setGlassItemsByBrand: () => {},
+  selectedGlasses: [],
+  setSelectedGlasses: () => {}
 };
 
-export const ContextDefaultValue: ContextProps = {
-  items: [
-    {
-      id: 1,
-      name: "Hugo Boss TEST 1",
-      description: "HB glasses model TEST1",
-      price: 800,
-      brand: {
-        brand_name: "Hugo Boss"
-      },
-      gender: {
-        gender_name: "Male"
-      },
-      type: {
-        type_name: "Eyeglasses"
-      }
-    },
-    {
-      id: 2,
-      name: "Hugo Boss TEST 2",
-      description: "HB glasses model TEST2",
-      price: 1000,
-      brand: {
-        brand_name: "Hugo Boss"
-      },
-      gender: {
-        gender_name: "Female"
-      },
-      type: {
-        type_name: "Eyeglasses"
-      }
-    }
-  ]
+export const AppContext = createContext<GlassContext>(ContextDefaultValue);
+
+export const useProduct = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("Error while reading context!");
+  }
+  return context;
 };
 
-export const AppContext = createContext<ContextProps>(EmptyContextDefaultValue);
-export const AppContextProvider = AppContext.Provider;
+
+export const AppContextProvider = ({ children }: {
+  children: React.ReactNode
+}) => {
+  // general state with al items
+  const [glassItems, setGlassItems] = useState<GlassItem[]>([]);
+
+
+  const [glassItemsByType, setGlassItemsByType] = useState<GlassType[]>([]);
+  const [glassItemsByGender, setGlassItemsByGender] = useState<GlassGender[]>([]);
+  const [glassItemsByBrand, setGlassItemsByBrand] = useState<GlassBrand[]>([]);
+  const [selectedGlasses, setSelectedGlasses] = useState<GlassSelections[]>([]);
+
+
+  return (
+    <AppContext.Provider
+      value={{
+        glassItems,
+        setGlassItems,
+        glassItemsByType,
+        setGlassItemsByType,
+        glassItemsByGender,
+        setGlassItemsByGender,
+        glassItemsByBrand,
+        setGlassItemsByBrand,
+        selectedGlasses,
+        setSelectedGlasses
+      }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+
 export const AppContextConsumer = AppContext.Consumer;
