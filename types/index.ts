4075c180837760b40356a70b11152b2fabe5c267
mariuszsak <mariuszsak@gmail.com>
2021-05-28 23:28:39 +0200

    Types added

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,29 @@ export type ItemButton = {
   title: string;
   onPress: () => void;
 }
+
+export type GlassBrand = {
+  brand_name: string;
+}
+
+export type GlassGender = {
+  gender_name: string;
+}
+
+export type GlassType = {
+  type_name: string;
+}
+
+export type GlassItem = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  brand: GlassBrand;
+  gender: GlassGender;
+  type: GlassType;
+}
+
+export type ProductType = { id: number } & GlassType;
+
+export type GlassSelections = GlassType | GlassBrand | GlassGender;
