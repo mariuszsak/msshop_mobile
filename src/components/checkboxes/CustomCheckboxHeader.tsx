afab4ff7b6bd11f3e5152d3962ced98a3d4dd199
mariuszsak <mariuszsak@gmail.com>
2021-05-31 21:44:20 +0200

    Checkbox header WIP

---
+++ src/components/checkboxes/CustomCheckboxHeader.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const CustomCheckboxHeader = ({ text }: any) => {
+
+  return (
+    <View>
+      <Text>{text}</Text>
+    </View>
+  );
+};
+
+export default CustomCheckboxHeader;
