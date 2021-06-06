35042353f4574e14f63e1f272654f50689adae29
mariuszsak <mariuszsak@gmail.com>
2021-06-06 22:38:25 +0200

    Added token, WIP

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,6 +1,10 @@
-export async function fetchData(endpoint: string): Promise<any> {
+export async function fetchData(endpoint: string, token: string): Promise<any> {
     const response: Response = await fetch(
-        `http://192.168.0.2:3000/${endpoint}`
+        `http://192.168.0.2:3000/${endpoint}`, {
+            headers: {
+                Authorization: 'Bearer ' + token
+            }
+      }
     );
     return response.json();
 }
