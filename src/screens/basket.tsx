85c15dabcbbe315804a56134430f6b9127ecb611
mariuszsak <mariuszsak@gmail.com>
2021-06-21 18:42:18 +0200

    WIP, style modified

--- src/screens/basket.tsx
+++ src/screens/basket.tsx
@@ -1,26 +1,28 @@
-import { View, StyleSheet, StatusBar } from 'react-native';
-import React from 'react';
-import Logo from '../components/logo';
-import { AppContext } from '../context/ItemContext';
-import BasketComponent from '../components/basketComponent';
+import { View, ScrollView } from "react-native";
+import React from "react";
+import Logo from "../components/logo";
+import { AppContext } from "../context/ItemContext";
+import BasketComponent from "../components/basketComponent";
 import styles from "../styles/style";
 
 export default function Basket() {
-    return (
-        <View style={styles.container}>
-            <StatusBar backgroundColor="#61dafb" hidden={false} />
-            <View style={styles.logo}>
-                <Logo />
-            </View>
-            <View style={styles.body}>
-                <AppContext.Consumer>
-                    {(context) =>
-                        context.glassItems.map((item, index) => (
-                            <BasketComponent name={item.name} key={index} />
-                        ))
-                    }
-                </AppContext.Consumer>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Logo />
+      </View>
+      <View style={styles.body}>
+        <ScrollView>
+          <AppContext.Consumer>
+            {(context) =>
+              context.glassItems.map((item, index) => (
+                <BasketComponent name={item.name} key={index} />
+              ))
+            }
+          </AppContext.Consumer>
+        </ScrollView>
+      </View>
+      <View style={styles.footer} />
+    </View>
+  );
 }
