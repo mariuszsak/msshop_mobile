ba25a9c37c38dbf67c2983155363500a1c01051a
mariuszsak <mariuszsak@gmail.com>
2021-06-21 19:08:13 +0200

    WIP, refactored

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { FlatList, ScrollView, TouchableOpacity, View } from "react-native";
 import ProductDetail from "../screens/productDetail";
 import styles from "../styles/style";
-import { MemoizedMediumImage } from "./MediumImage";
-import { MemoizedShortInfo } from "./ShortInfo";
+import BookmarkButton from "./bookmark/BookmarkButton";
+import ShortItem from "./item/ShortItem";
 
 type Props = {
   onPress(): void;
@@ -16,32 +16,36 @@ export function ItemList(props: any) {
     navigate("ProductDetail", item);
   }
 
+  function onPress() {
+    console.log("bookmarked!");
+  }
+
   return (
     <View style={styles.container}>
-      <FlatList
-        contentContainerStyle={styles.allItems}
-        data={props.data}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) => (
-          <TouchableOpacity
-            style={styles.singleItem}
-            onPress={() => goDetail(item)}
-          >
-            <View style={styles.left}>
-              <MemoizedMediumImage id={item.id} />
-            </View>
-            <View style={styles.right}>
-              <MemoizedShortInfo
-                // TODO name is unnecessary
-                name={item.brand.brand_name}
-                price={item.price}
-                brand={item.brand.brand_name}
-                type={item.type.type_name}
-              />
-            </View>
-          </TouchableOpacity>
-        )}
-      />
+      <ScrollView horizontal={false}>
+        <FlatList
+          contentContainerStyle={styles.allItems}
+          data={props.data}
+          numColumns={2}
+          horizontal={false}
+          keyExtractor={(i, index) => index.toString()}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity
+              style={styles.singleItem}
+              onPress={() => goDetail(item)}
+            >
+              <ShortItem item={item} />
+              <TouchableOpacity
+                onPress={() => onPress()}
+                style={
+                  styles.bookmark
+                }>
+                <BookmarkButton />
+              </TouchableOpacity>
+            </TouchableOpacity>
+          )}
+        />
+      </ScrollView>
     </View>
   );
 }
