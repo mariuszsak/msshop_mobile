10221c60ac75de0399a7718203c3844bbdddbb37
mariuszsak <mariuszsak@gmail.com>
2021-06-21 19:12:47 +0200

    Component added

--- src/components/text/DetailText.tsx
+++ src/components/text/DetailText.tsx
@@ -5,14 +5,20 @@ import { useFonts } from "expo-font";
 const DetailText = (props: any) => {
   const [loaded] = useFonts({
     "Roboto": require("../../../assets/Roboto-BlackItalic.ttf"),
-    'Ionicons': require("../../../node_modules/react-native-vector-icons/Fonts/Ionicons.ttf")
+    "RobotoSlab": require("../../../assets/RobotoSlab-Regular.ttf"),
+    "Ionicons": require("../../../node_modules/react-native-vector-icons/Fonts/Ionicons.ttf")
   });
 
   if (!loaded) {
     return null;
   } else {
     return (
-      <Text style={{fontFamily: 'Roboto', fontWeight: 'normal', fontSize: 14}}>
+      <Text style={{
+        fontFamily: "RobotoSlab",
+        fontWeight: "normal",
+        fontSize: 24
+      }}
+      >
         {props.children}
       </Text>
     );
