fd77a39eb9b93f49ab559095c91b86d1c856dda3
mariuszsak <mariuszsak@gmail.com>
2021-07-02 01:29:45 +0200

    Style fixed, WIP

--- src/screens/info.tsx
+++ src/screens/info.tsx
@@ -1,48 +1,39 @@
-import { View, StyleSheet, Dimensions } from 'react-native';
-import React from 'react';
-import Logo from '../components/logo';
+import { View, StyleSheet, Dimensions } from "react-native";
+import React from "react";
+import Logo from "../components/logo";
 import MapView, { Marker } from "react-native-maps";
+import styles from "../styles/style";
+
 
 export default function Info() {
-    return (
-        <View style={styles.container}>
-            <View>
-                <Logo />
-            </View>
-            <View style={styles.body}>
-              <View style={styles.container}>
-                <MapView style={styles.map}  initialRegion={{
-                  latitude: 52.229676,
-                  longitude: 21.012229,
-                  latitudeDelta: 0.1,
-                  longitudeDelta: 0.1,
-                }}>
-                <Marker coordinate={{
-                  latitude: 52.229676,
-                  longitude: 21.012229
-                }}
-                title={'GlassShop'}
-                description={'00-001 Warszawa, ul. Okularowa 1'}/>
-                </MapView>
-              </View>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Logo />
+      </View>
+      <View style={styles.body}>
+        <MapView style={styless.map} initialRegion={{
+          latitude: 52.229676,
+          longitude: 21.012229,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1
+        }}>
+          <Marker coordinate={{
+            latitude: 52.229676,
+            longitude: 21.012229
+          }}
+                  title={"GlassShop"}
+                  description={"00-001 Warszawa, ul. Okularowa 1"} />
+        </MapView>
+      </View>
+      <View style={styles.footer} />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    header: {
-        flex: 1,
-        backgroundColor: 'pink'
-    },
-    body: {
-        flex: 9
-    },
+const styless = StyleSheet.create({
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
+    width: "100%",
+    height: "100%"
+  }
 });
