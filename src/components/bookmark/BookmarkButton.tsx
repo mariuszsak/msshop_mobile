e99f5a5b6f357b582d1b62efb0695928c3eca92e
mariuszsak <mariuszsak@gmail.com>
2021-07-02 01:27:20 +0200

    WIP

--- src/components/bookmark/BookmarkButton.tsx
+++ src/components/bookmark/BookmarkButton.tsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { Text, View } from "react-native";
-import styles from "../../styles/style";
+import React, { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+
+interface IBookmarkButton {
+  isBookmarked: boolean;
+}
 
 const BookmarkButton = () => {
 
+  const [isBmk, setIsBmk] = useState(false);
+
   return (
-    <View>
+    <TouchableOpacity onPress={() => {
+      setIsBmk(!isBmk);
+    }}>
       <Text>
-        <Icon name="bookmark-outline" size={22} color={"white"} />
+        <Icon name="bookmark" size={22} color={isBmk ? "red" : "white"} />
       </Text>
-    </View>
+    </TouchableOpacity>
 
   );
 };
