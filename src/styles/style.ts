596d34c39330377ec2b810f9130bef6fe6bb629a
mariuszsak <mariuszsak@gmail.com>
2021-07-02 01:34:24 +0200

    WIP

--- src/styles/style.ts
+++ src/styles/style.ts
@@ -1,69 +1,97 @@
 import { StyleSheet } from "react-native";
+import { StatusBar } from "react-native";
+
+const bgcolor = "#fff";
+const buttonColor = "";
+const menucolor = "";
 
 const styles = StyleSheet.create({
   //wtf
   container: {
-    flex:1
+    flex: 1,
+    marginTop: StatusBar.currentHeight
   },
-  //logo style
+  header: {
+    flex: 2
+  },
+  body: {
+    flex: 15,
+    backgroundColor: bgcolor
+    // margin: 10
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: bgcolor
+  },
   logo: {
     flex: 1,
-    paddingTop:10,
     width: "100%",
     resizeMode: "contain",
-    backgroundColor: '#fff'
+    backgroundColor: bgcolor
   },
   // FlatList
   allItems: {
-    backgroundColor: "#fff",
-    margin: 15
+    margin: 0,
+    padding: 0
   },
   singleItem: {
-    paddingTop: 5,
-    paddingBottom: 5,
-    marginTop: 5,
-    flexDirection: "row",
-    borderStyle: "solid",
-    // borderWidth: 1,
-    // borderColor: "red",
-    height: 150
-  },
-  left: {
-    // width: "50%",
-    // borderWidth: 1
-  },
-  right: {
-    // flexDirection: "column",
-    justifyContent: "flex-start",
-    paddingLeft: 35,
-    paddingTop: 35
+    margin: 5,
+    borderWidth: 1,
+    borderColor: "#d9d9d9",
+    borderRadius: 30,
+    height: 160
   },
   name: {
-    // fontFamily: 'Roboto',
-    // fontWeight: "bold",
-    color: '#FF7657',
-    fontWeight: 'bold',
-    fontSize: 25
+    // fontFamily: "Roboto",
+    left: 10,
+    fontWeight: "bold",
+    color: "#FF7657",
+    fontSize: 20
   },
   type: {
+    left: 10,
     fontSize: 15
   },
   price: {
+    left: 10,
     paddingTop: 5,
     fontWeight: "bold",
     fontSize: 15
+  },
+  priceDetail: {
+    paddingBottom: 25,
+    fontWeight: "bold",
+    fontSize: 25,
+    color: "#007AFF"
+  },
+  description: {
+    paddingTop: 5,
+    fontSize: 15,
+    paddingBottom: 20
   },
   checkboxLabel: {
     fontSize: 10
   },
   mediumImage: {
+    margin: 5,
     width: 150,
-    height: 150
+    height: 55
+  },
+  bigImage: {
+    width: 330,
+    height: 156,
+    margin: 0,
+    padding: 0,
+    top: 0,
+    left: 0
   },
   detail: {
-    borderWidth: 0,
-    padding: 0,
-    justifyContent: "center",
+    // borderWidth: 1,
+    borderColor: "red",
+    paddingBottom: 15,
+    // justifyContent: "flex-start",
+    // flexDirection: "column"
+    flex: 2,
     alignItems: "center"
   },
   quantityContainer: {
@@ -76,31 +104,46 @@ const styles = StyleSheet.create({
   },
   buttonsContainer: {
     flexDirection: "row",
-    justifyContent: 'space-between',
+    justifyContent: "space-between"
   },
-    checkboxes: {
+  checkboxes: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "flex-end"
   },
-  body: {
-    flex: 9,
-    backgroundColor: "#fff"
-  },
   bor: {
-    flex: 1,
-    borderStyle: "solid",
-    borderWidth: 1,
-    borderColor: "black"
+    // flex: 1,
+    // borderStyle: "solid",
+    // borderWidth: 1,
+    // borderColor: "black"
   },
-    btn: {
-    padding: 5,
-    backgroundColor: "#fff"
+  btn: {
+    backgroundColor: "#888"
+    // borderWidth: 1,
+    // borderRadius: 15
   },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1
+  },
+  bookmark: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 40,
+    width: 40,
+    position: "absolute",
+    borderRadius: 15,
+    backgroundColor: "lightgray",
+    bottom: -5,
+    right: -5
+  },
+  addRemoveContainer: {
+    flex: 3
+    // position: "absolute",
+    // bottom: 0,
+
   }
 });
 
