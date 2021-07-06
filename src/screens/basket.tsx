49ed5ae77406916b13d57dd2cfa78c96533b9d2c
mariuszsak <mariuszsak@gmail.com>
2021-07-06 12:19:33 +0200

    WIP, component added

--- src/screens/basket.tsx
+++ src/screens/basket.tsx
@@ -1,26 +1,21 @@
-import { View, ScrollView } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import Logo from "../components/logo";
-import { AppContext } from "../context/ItemContext";
-import BasketComponent from "../components/basketComponent";
 import styles from "../styles/style";
+import { useBasket } from "../context/BasketContext";
+import { BasketList } from "../components/BasketList";
 
 export default function Basket() {
+  const { currentBasket } = useBasket();
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Logo />
       </View>
       <View style={styles.body}>
-        <ScrollView>
-          <AppContext.Consumer>
-            {(context) =>
-              context.glassItems.map((item, index) => (
-                <BasketComponent name={item.name} key={index} />
-              ))
-            }
-          </AppContext.Consumer>
-        </ScrollView>
+        <BasketList
+          data={currentBasket}
+        />
       </View>
       <View style={styles.footer} />
     </View>
