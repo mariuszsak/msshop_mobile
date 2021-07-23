9b37de3f561e4a720575acb46a35625b4e1b00ea
mariuszsak <mariuszsak@gmail.com>
2021-07-23 16:02:20 +0200

    Some fixes, style & import mainly

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { FlatList, TouchableOpacity, View } from "react-native";
-import ProductDetail from "../screens/productDetail";
-import styles from "../styles/style";
+import { FlatList, StyleSheet, TouchableOpacity, View } from "react-native";
+import { DetailScreen } from "../screens/DetailScreen";
 import BookmarkButton from "./bookmark/BookmarkButton";
-import ShortItem from "./item/ShortItem";
+import { ShortItem } from "./item/ShortItem";
 import { BookmarkedItem } from "../../types";
 
 type Props = {
@@ -14,9 +13,8 @@ export const ItemList = (props: BookmarkedItem[] | any) => {
 
   function goDetail(item: Props) {
     const { navigate } = props.navigation;
-    navigate("ProductDetail", item);
+    navigate("DetailScreen", item);
   }
-
 
   const element = ({ item }: any) => {
     return (
@@ -24,7 +22,9 @@ export const ItemList = (props: BookmarkedItem[] | any) => {
         style={styles.singleItem}
         onPress={() => goDetail(item)}
       >
-        <ShortItem item={item} />
+        <View style={{width: '70%'}}>
+          <ShortItem item={item} />
+        </View>
         <View
           style={
             styles.bookmark
@@ -40,10 +40,36 @@ export const ItemList = (props: BookmarkedItem[] | any) => {
       <FlatList
         contentContainerStyle={styles.allItems}
         data={props.data}
-        numColumns={2}
-        keyExtractor={item => item.id}
+        numColumns={1}
+        keyExtractor={item => item.id.toString()}
         renderItem={element}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  singleItem: {
+    marginTop: 5,
+    borderWidth: 1,
+    borderColor: "#d9d9d9",
+    borderRadius: 30,
+    height: 160
+  },
+  allItems: {
+    margin: 0,
+    padding: 0
+  },
+  bookmark: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    width: 50,
+    position: "absolute",
+    borderRadius: 15,
+    backgroundColor: "lightgray",
+    bottom: -5,
+    right: 0
+  }
+});
