9f7a2db508aad30d94699cb2867dedd5d095b0cb
mariuszsak <mariuszsak@gmail.com>
2021-08-02 13:48:06 +0200

    WIP, added missing await

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export async function fetchData(endpoint: string, token?: string): Promise<any> {
 
-  return axios.get(`http://${DB_HOST}/${endpoint}`)
+  return await axios.get(`http://${DB_HOST}/${endpoint}`)
     .then((response) => response.data);
 }
