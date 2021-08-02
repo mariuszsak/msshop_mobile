349b8e369ea2f4837ef30fe83e276c7edca472d8
mariuszsak <mariuszsak@gmail.com>
2021-08-03 00:39:25 +0200

    Styles & props fixed

--- src/components/ShortInfo.tsx
+++ src/components/ShortInfo.tsx
@@ -1,30 +1,48 @@
 import React, { memo } from "react";
-import { Text, View } from "react-native";
-import styles from "../styles/style";
+import { StyleSheet, Text, View } from "react-native";
 
 interface ShortInfoProps {
   name: string;
   price: string;
-  type: string
+  type: string;
 }
 
 export const ShortInfo = memo((props: ShortInfoProps) => {
-  const {name, price, type} = props;
+  const { name, price, type } = props;
   return (
     <>
       <View>
         <Text style={styles.name}>
-         {name}
+          {name}
         </Text>
       </View>
       <View>
-          <Text style={styles.type}>
-            {type}
-          </Text>
-          <Text style={styles.price}>
-            {price}PLN
-          </Text>
+        <Text style={styles.type}>
+          {type}
+        </Text>
+        <Text style={styles.price}>
+          {price}PLN
+        </Text>
       </View>
     </>
   );
+});
+
+const styles = StyleSheet.create({
+  name: {
+    left: 10,
+    fontWeight: "bold",
+    color: "#FF7657",
+    fontSize: 20
+  },
+  type: {
+    left: 10,
+    fontSize: 15
+  },
+  price: {
+    left: 10,
+    paddingTop: 5,
+    fontWeight: "bold",
+    fontSize: 15
+  }
 });
